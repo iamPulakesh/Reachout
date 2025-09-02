74aa1d0c5ea77bd8a184035069e17b671951d3e0
Pulakesh Mondal <pulak4467@gmail.com>
2025-09-02 21:04:57 +0530

    AWS_RDS to Local_MySQL migration

--- db/connection.py
+++ db/connection.py
@@ -1,14 +1,19 @@
+import os
 import boto3
 import mysql.connector
+from dotenv import load_dotenv
+
+load_dotenv()
 
 def get_ssm_parameter(name, secure=False):
     ssm = boto3.client('ssm', region_name='ap-south-1')
     return ssm.get_parameter(Name=name, WithDecryption=secure)['Parameter']['Value']
 
-DB_HOST = get_ssm_parameter("/reachout/DB_HOST")
-DB_USER = get_ssm_parameter("/reachout/DB_USER")
-DB_PASSWORD = get_ssm_parameter("/reachout/DB_PASSWORD", secure=True)
-DB_NAME = get_ssm_parameter("/reachout/DB_NAME")
+# DB_HOST = get_ssm_parameter("/reachout/DB_HOST")
+# DB_USER = get_ssm_parameter("/reachout/DB_USER")
+# DB_PASSWORD = get_ssm_parameter("/reachout/DB_PASSWORD", secure=True)
+# DB_NAME = get_ssm_parameter("/reachout/DB_NAME")
+
 GOOGLE_MAPS_API_KEY = get_ssm_parameter("/reachout/GOOGLE_MAPS_API_KEY", secure=True)
 S3_BASE_URL = get_ssm_parameter("/reachout/S3_BASE_URL")
 BUCKET = get_ssm_parameter("/reachout/S3_BUCKET")
@@ -17,8 +22,8 @@ s3 = boto3.client('s3', region_name='ap-south-1')
 
 def get_db_connection():
     return mysql.connector.connect(
-        host=DB_HOST,
-        user=DB_USER,
-        password=DB_PASSWORD,
-        database=DB_NAME
+        host=os.getenv("DB_HOST"),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD"),
+        database=os.getenv("DB_NAME")
     )
